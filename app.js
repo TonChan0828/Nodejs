47082611eaec9470afdac6dec8cbf782eab151c7
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-04-27 08:25:11 +0900

    表示内容をHTMLファイルに分離

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 var server = http.createServer(
     (require, response) => {
-        response.setHeader('Control-Type', 'text/html');
-        response.write('<!DOCTYPE html><html lang="ja">');
-        response.write('<head><meta charset="utf-8">');
-        response.write('<title>Hello</title></head>');
-        response.write('<body><h1>Hello Node.js!</h1>');
-        response.write('<p>This is Node.js sample page.</p>');
-        response.write('<p>これはNode.jsのサンプルページです。</p>','utf8');
-        response.write('</body></html>');
-        response.end();
+        fs.readFile('./index.html', 'UTF-8', (error, data) => {
+            response.writeHead(200, { 'Content-Type': 'text.html' });
+            response.write(data);
+            response.end();
+        });
     }
 );
 
 server.listen(3000);
+console.log('Server start!');
