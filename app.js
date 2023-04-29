edcec8b4282eccde54f77d5f0512896624bebaa7
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-04-29 11:52:48 +0900

    CreateServerの関数の切り離し

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-var server = http.createServer(
-    (require, response) => {
-        fs.readFile('./index.html', 'UTF-8', (error, data) => {
-            response.writeHead(200, { 'Content-Type': 'text.html' });
-            response.write(data);
-            response.end();
-        });
-    }
-);
+var server = http.createServer(getFormClient);
 
 server.listen(3000);
-console.log('Server start!');
+console.log("Server start!");
+
+function getFormClient(req, res) {
+  request = req;
+  response = res;
+  fs.readFile("./index.html", "UTF-8", (error, data) => {
+    response.writeHead(200, { "Content-Type": "text.html" });
+    response.write(data);
+    response.end();
+  });
+}
