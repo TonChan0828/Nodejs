63a826fbd422a5912e7cc8f654367b5469ef67d6
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-05-23 22:35:19 +0900

    feat データベースの値を表示

--- express/ex-gen-app/routes/hello.js
+++ express/ex-gen-app/routes/hello.js
@@ -1,29 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const http = require("https");
-const parseString = require("xml2js").parseString;
 
+const sqlite3 = require("sqlite3");
+
+// データベースオブジェクトの取得
+const db = new sqlite3.Database("mydb.sqlite3");
+
+// GETアクセスの処理
 router.get("/", (req, res, next) => {
-  var opt = {
-    host: "news.google.com",
-    port: 443,
-    path: "/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja",
-  };
-  http.get(opt, (res2) => {
-    var body = "";
-    res2.on("data", (data) => {
-      body += data;
-    });
-    res2.on("end", () => {
-      parseString(body.trim(), (err, result) => {
-        console.log(result);
-        var data = {
-          title: "Google News",
-          content: result.rss.channel[0].item,
-        };
-        res.render("hello", data);
-      });
-    });
+  // データベースのシリアライズ
+  db.serialize(() => {});
+  // レコードをすべて取り出す
+  db.all("select * from mydata", (err, rows) => {
+    // データベースアクセス完了時の処理
+    if (!err) {
+      var data = {
+        title: "Hello!",
+        content: rows,
+      };
+      res.render("hello", data);
+    }
   });
 });
 
