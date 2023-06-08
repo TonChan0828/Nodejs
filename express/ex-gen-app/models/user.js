005b84533d3e503bbfeb81c3eddf4243b8e63f46
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-06-08 23:08:21 +0900

    add: バリデーション処理を追加

--- express/ex-gen-app/models/user.js
+++ express/ex-gen-app/models/user.js
@@ -1,26 +1,33 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    name: DataTypes.STRING,
-    pass: DataTypes.STRING,
-    mail: DataTypes.STRING,
-    age: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: { msg: "名前を必ず入力してください" } },
+      },
+      pass: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: { msg: "パスワードを必ず入力してください" } },
+      },
+      mail: {
+        type: DataTypes.STRING,
+        validate: { isEmail: { msg: "メールアドレスを必ず入力してください" } },
+      },
+      age: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: { msg: "整数を入力してください" },
+          min: { args: [0], msg: "0以上の値が必要です" },
+        },
+      },
+    },
+    {}
+  );
+  User.associate = function (models) {
+    // associations can be defined here
+  };
   return User;
 };
--- express/ex-gen-app/routes/users.js
+++ express/ex-gen-app/routes/users.js
@@ -19,24 +19,33 @@ router.get("/", function (req, res, next) {
 router.get("/add", (req, res, next) => {
   var data = {
     title: "Users/Add",
+    form: new db.User(),
+    err: null,
   };
   res.render("users/add", data);
 });
 
 router.post("/add", (req, res, next) => {
-  db.sequelize
-    .sync()
-    .then(() =>
-      db.User.create({
-        name: req.body.name,
-        pass: req.body.pass,
-        mail: req.body.mail,
-        age: req.body.age,
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age,
+  };
+  db.sequelize.sync().then(() =>
+    db.User.create(form)
+      .then((users) => {
+        res.redirect("/users");
       })
-    )
-    .then((users) => {
-      res.redirect("/users");
-    });
+      .catch((err) => {
+        var data = {
+          title: "Users/Add",
+          form: form,
+          err: err,
+        };
+        res.render("users/add", data);
+      })
+  );
 });
 
 router.get("/edit", (req, res, next) => {
