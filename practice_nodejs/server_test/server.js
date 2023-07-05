c61ade80246b32313a549c7e0ab4d399a7df7305
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-05 23:02:29 +0900

    add:包括的エラーハンドリングを追加

--- practice_nodejs/server_test/server.js
+++ practice_nodejs/server_test/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello world!");
+  res.status(200).send("hello world!\n");
 });
 
 app.get("/user/:id", (req, res) => {
   res.status(200).send(req.params.id);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send("Internal Server Error");
 });
 
 app.listen(3000, () => {
