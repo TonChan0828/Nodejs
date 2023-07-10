f4d8d84152e0dae495e0b99e6300053622c5a889
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-10 23:26:20 +0900

    add:ビューテンプレートの実装

--- practice_nodejs/server_test/server.js
+++ practice_nodejs/server_test/server.js
@@ -1,59 +1,13 @@
-const Redis = require("ioredis");
+const path = require("path");
 const express = require("express");
 const app = express();
 
-const redis = new Redis({
-  port: 6379,
-  host: "localhost",
-  password: process.env.REDIS_PASSWORD,
-});
-
-const init = async () => {
-  await redis.rpush("users:list", JSON.stringify({ id: 1, name: "alpha" }));
-  await redis.rpush("users:list", JSON.stringify({ id: 2, name: "bravo" }));
-  await redis.rpush("users:list", JSON.stringify({ id: 3, name: "charlie" }));
-  await redis.rpush("users:list", JSON.stringify({ id: 4, name: "delta" }));
-};
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello world");
+  res.render(path.join(__dirname, "views", "index.ejs"));
 });
 
-app.get("/user/:id", async (req, res) => {
-  try {
-    const key = `users:${req.params.id}`;
-    const val = await redis.get(key);
-    const user = JSON.parse(val);
-    res.status(200).json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("internal error");
-  }
-});
-
-app.get("/users", async (req, res) => {
-  const offset = req.query.offset ? Number(req.query.offset) : 0;
-  const usersList = await redis.lrange("users:list", offset, offset + 1);
-  const users = usersList.map((user) => {
-    return JSON.parse(user);
-  });
-  console.log(users);
-  return { users: users };
-});
-
-redis.once("ready", async () => {
-  try {
-    await init();
-    app.listen(3000, () => {
-      console.log("start listening");
-    });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-});
-
-redis.on("error", (err) => {
-  console.error(err);
-  process.exit(1);
+app.listen(3000, () => {
+  console.log("start listening");
 });
