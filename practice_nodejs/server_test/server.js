690015ca9f203a02dc177e08ba6fbfc2cbd0bb3c
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-12 22:28:54 +0900

    add:ビューテンプレートを追加

--- practice_nodejs/server_test/server.js
+++ practice_nodejs/server_test/server.js
@@ -1,13 +1,64 @@
-const path = require("path");
+const Redis = require("ioredis");
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render(path.join(__dirname, "views", "index.ejs"));
+const redis = new Redis({
+  port: 6379,
+  host: "localhost",
+  password: process.env.REDIS_PASSWORD,
 });
 
-app.listen(3000, () => {
-  console.log("start listening");
+const init = async () => {
+  await Promise.all([
+    redis.set("users:1", JSON.stringify({ id: 1, name: "alpha" })),
+    redis.set("users:2", JSON.stringify({ id: 2, name: "bravo" })),
+    redis.set("users:3", JSON.stringify({ id: 3, name: "charlie" })),
+    redis.set("users:4", JSON.stringify({ id: 4, name: "delta" })),
+  ]);
+};
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
+
+app.get("/user/:id", (req, res) => {
+  res.status(200).send(req.params.id);
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const stream = redis.scanStream({
+      match: "users:*",
+      count: 2,
+    });
+    const users = [];
+    for await (const resultKeys of stream) {
+      for (const key of resultKeys) {
+        const value = await redis.get(key);
+        const user = JSON.parse(value);
+        users.push(user);
+      }
+    }
+    res.render(path.join(__dirname, "views", "users.ejs"), { users: users });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+redis.once("ready", async () => {
+  try {
+    await init();
+    app.listen(3000, () => {
+      console.log("start listening");
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+redis.on("error", (err) => {
+  console.error(err);
+  process.exit(1);
 });
